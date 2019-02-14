9ddc51b130b483aa6e1401b572aa63e96994732a
17338 <zakbukarsa@gmail.com>
2019-02-14 11:15:57 +0100

    “Add more tests

--- test_utils.py
+++ test_utils.py
@@ -7,12 +7,17 @@ import utils
 
 class TestUtils(unittest.TestCase):
     def test_fact(self):
+        self.assertEqual(utils.fact(5),120)
+        self.assertEqual(utils.fact(0),1)
+        with self.assertRaises(ValueError):
+            utils.fact(-5)
         # À compléter...
-        pass
     
     def test_roots(self):
         # À compléter...
-        pass
+        self.assertEqual(utils.roots(1,2,1),(-1.0))
+        self.assertEqual(utils.roots(1,1,1),())
+        self.assertEqual(utils.roots(1,-1,-6),(3.0,-2.0))
     
     def test_integrate(self):
         # À compléter...
