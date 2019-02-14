94f4b46add7989f92250516393202cea830e6b1e
17338 <zakbukarsa@gmail.com>
2019-02-14 11:15:36 +0100

    Fix utils module to pass tests

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@
 # Author: Sébastien Combéfis
 # Version: February 8, 2018
 
+
 def fact(n):
 	"""Computes the factorial of a natural number.
 	
@@ -10,7 +11,12 @@ def fact(n):
 	Post: Returns the factorial of 'n'.
 	Throws: ValueError if n < 0
 	"""
-	pass
+	if n < 0 :
+		raise ValueError ("Must be positif") 
+	if n == 0 : 
+		return 1 
+	else : 
+		return (n*fact(n-1))
 
 def roots(a, b, c):
 	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -19,7 +25,14 @@ def roots(a, b, c):
 	Post: Returns a tuple with zero, one or two elements corresponding
 		to the roots of the ax^2 + bx + c polynomial.
 	"""
-	pass
+	delta = b**2-(4*a*c)
+	if delta < 0 :
+		result = ()
+	if delta ==0 :
+		result = (-b/(2*a))
+	if delta >0 : 
+		result = ((-b+(delta**0.5))/(2*a),(-b-(delta**0.5))/(2*a))
+	return result 
 
 def integrate(function, lower, upper):
 	"""Approximates the integral of a fonction between two bounds
